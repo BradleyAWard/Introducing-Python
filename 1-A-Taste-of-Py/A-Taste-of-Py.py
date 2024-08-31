7fd64151f25ba40ad96d1501f8dbd26597b9337b
bradleyward <wardb2@cardiff.ac.uk>
2024-08-31 22:09:02 +0100

    Chapter 1

---
+++ 1-A-Taste-of-Py/A-Taste-of-Py.py
@@ -0,0 +1,10 @@
+# Code 1
+import this
+
+# Code 2
+quotes = {"Moe": "A wise guy, huh?",
+        "Larry": "Ow!",
+        "Curly": "Nyuk, nyuk!"}
+
+stooge = "Curly"
+print(stooge, "says:", quotes[stooge])
